e27d64002a58fbe40e8bc702d8f706d9d01bf721
Philipp <philipp.lange23@gmail.com>
2018-04-07 10:40:19 +0200

    Initial commit

---
+++ app/src/main/java/de/plitsolutions/schorni2/controller/OpenJobsActivity.kt
@@ -0,0 +1,58 @@
+package de.plitsolutions.schorni2.controller
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import de.plitsolutions.schorni2.R
+import kotlinx.android.synthetic.main.activity_open_jobs.*
+import kotlinx.android.synthetic.main.app_bar_open_jobs.*
+
+class OpenJobsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_jobs)
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_open_jobs -> {
+                // Handle the camera action
+            }
+            R.id.nav_finished_jobs -> {
+
+            }
+            R.id.nav_export -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
